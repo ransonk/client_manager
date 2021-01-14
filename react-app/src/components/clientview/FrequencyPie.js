dc8115abf862a72a3845b6abc781e3eba1750c6e
Ranson Knorr <ransonknorr@gmail.com>
2021-01-14 00:56:54 -0600

    set new color scheme to match exercise targeter and frequency pie chart

--- react-app/src/components/clientview/FrequencyPie.js
+++ react-app/src/components/clientview/FrequencyPie.js
@@ -24,31 +24,80 @@ function FrequencyPie() {
 
     const historicalDataRaw = useSelector((state) => state.store.client_progress)
     const historicalData = Object.values(historicalDataRaw)
-    // console.log('his', historicalData)
+    console.log('historical', historicalData)
     const sortedData = historicalData.sort((a, b) => new Date(a.date) - new Date(b.date))
-    // console.log('sort', sortedData)
+
     const dates = sortedData.map(history => history.date)
-    // console.log('dates', dates)
-    //DATES ARE NOW SORTED, YAY! MAP THROUGH THIS DATA BELOW FOR GRAPHICAL REPRESENTATION
 
     let pushCount = sortedData.map(history => Number(history.pushCount))
     let pullCount = sortedData.map(history => Number(history.pullCount))
     pushCount = pushCount.reduce((a, b) => a + b, 0)
     pullCount = pullCount.reduce((a, b) => a + b, 0)
-    // console.log('Push freq ', pushCount, 'Pull freq ', pullCount)
+
+    let exerciseCounter = {};
+
+    historicalData.forEach(record => {
+        if (!exerciseCounter[record.workout1]) {
+            exerciseCounter[record.workout1] = 1
+        } else {
+            exerciseCounter[record.workout1] = exerciseCounter[record.workout1] + 1;
+        }
+
+        if (!exerciseCounter[record.workout2]) {
+            exerciseCounter[record.workout2] = 1
+        } else {
+            exerciseCounter[record.workout2] = exerciseCounter[record.workout2] + 1;
+        }
+
+        if (!exerciseCounter[record.workout3]) {
+            exerciseCounter[record.workout3] = 1
+        } else {
+            exerciseCounter[record.workout3] = exerciseCounter[record.workout3] + 1;
+        }
+
+        if (!exerciseCounter[record.workout4]) {
+            exerciseCounter[record.workout4] = 1
+        } else {
+            exerciseCounter[record.workout4] = exerciseCounter[record.workout4] + 1;
+        }
+
+        if (!exerciseCounter[record.workout5]) {
+            exerciseCounter[record.workout5] = 1
+        } else {
+            exerciseCounter[record.workout5] = exerciseCounter[record.workout5] + 1;
+        }
+
+        if (!exerciseCounter[record.workout6]) {
+            exerciseCounter[record.workout6] = 1
+        } else {
+            exerciseCounter[record.workout6] = exerciseCounter[record.workout6] + 1;
+        }
+
+        if (!exerciseCounter[record.workout7]) {
+            exerciseCounter[record.workout7] = 1
+        } else {
+            exerciseCounter[record.workout7] = exerciseCounter[record.workout7] + 1;
+        }
+
+        if (!exerciseCounter[record.workout8]) {
+            exerciseCounter[record.workout8] = 1
+        } else {
+            exerciseCounter[record.workout8] = exerciseCounter[record.workout8] + 1;
+        }
+    })
+
+    console.log('exercise counter', exerciseCounter)
+    let borderColors = ['#f94144', '#f3722c', '#f8961e', '#f9c74f', '#90be6d', "#43aa8b", "#577590", "#0081a7", "#fed9b7", '#f07167', "#00f5d4", "#d0f4de", "#c8553d", "#b09e99"]
 
 
     const data = {
 
         datasets: [{
-            data: [pushCount, pullCount],
-            backgroundColor: ['#f28482', '#99c1de'],
+            data: Object.values(exerciseCounter),
+            backgroundColor: borderColors,
             border: 'none'
         }],
-        labels: [
-            'Push Frequency',
-            'Pull Frequency'
-        ]
+        labels: Object.keys(exerciseCounter),
     }
 
     return (
