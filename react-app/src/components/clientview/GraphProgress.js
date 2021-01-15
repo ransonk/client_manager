4bc67f357d5cb79f19d6b29b2258e51f632d23b8
Ranson Knorr <ransonknorr@gmail.com>
2021-01-14 18:10:07 -0600

    fix targeted exercise progression lack of null inputs, graph now reflects days with no workout in specific exercises

--- react-app/src/components/clientview/GraphProgress.js
+++ react-app/src/components/clientview/GraphProgress.js
@@ -97,15 +97,76 @@ function GraphProgress() {
     })
 
 
-    let exerciseRecordList = Object.entries(exerciseRecord)
+    const dates = sortedData.map(history => history.date)
 
+    dates.forEach(date => {
+
+        for (let key in exerciseRecord) {
+            if (!exerciseRecord[key].includes(date)) {
+                exerciseRecord[key] = [...exerciseRecord[key], date, 0]
+
+
+            }
+        }
+
+    })
+
+    for (let key in exerciseRecord) {
+        let unOrganizedDates = exerciseRecord[key]
+        let organizedDates = []
+        let finalDates = []
+        console.log('LOOKIE HERE SON', unOrganizedDates)
+
+        for (let i = 0; i < unOrganizedDates.length - 1; i += 2) {
+            organizedDates.push({ date: unOrganizedDates[i], score: unOrganizedDates[i + 1] })
+        }
+        organizedDates.sort((a, b) => new Date(a.date) - new Date(b.date))
+        console.log('new Object? ', organizedDates)
+
+        for (let i = 0; i < organizedDates.length; i++) {
+            let date = organizedDates[i].date
+            let score = organizedDates[i].score
+            finalDates.push(date)
+            finalDates.push(score)
+        }
+        console.log('final Object? ', finalDates)
+        exerciseRecord[key] = finalDates
+
+    }
+
+
+    let exerciseRecordList = Object.entries(exerciseRecord)
     let datasetRecords = []
     let borderColors = ['#f94144', '#f3722c', '#f8961e', '#f9c74f', '#90be6d', "#43aa8b", "#577590", "#0081a7", "#fed9b7", '#f07167', "#00f5d4", "#d0f4de", "#c8553d", "#b09e99"]
+
+    // dates.forEach(date => {
+
+    //     exerciseRecordList.forEach(dateList => {
+    //         if (!dateList.includes(date)) {
+    //             dateList.push(date)
+    //             dateList.push(0)
+    //         }
+    //     })
+
+    // })
+
+    // dates.forEach(date => {
+
+    //     for (let key in exerciseRecord) {
+    //         if (!exerciseRecord[key].includes(date)) {
+    //             exerciseRecord[key] = [...exerciseRecord[key], date, 0]
+
+
+    //         }
+    //     }
+
+    // })
 
 
     exerciseRecordList.map((item, i) => {
         let scoreList = [];
         item[1].forEach(num => {
+            console.log('nummm', num)
             if (typeof (num) == 'number') {
                 scoreList.push(num)
             }
@@ -131,9 +192,66 @@ function GraphProgress() {
 
 
 
-    const dates = sortedData.map(history => history.date)
-    console.log('dates', dates)
-    console.log('data set records????', datasetRecords)
+    // const dates = sortedData.map(history => history.date)
+    // console.log('dates', dates)
+    // console.log('data set records????', datasetRecords)
+
+
+    // dates.forEach(date => {
+
+    //     exerciseRecordList.forEach(dateList => {
+    //         if (!dateList.includes(date)) {
+    //             dateList.push(date)
+    //             dateList.push(0)
+    //         }
+    //     })
+
+    // })
+
+    // dates.forEach(date => {
+
+    //     for (let key in exerciseRecord) {
+    //         if (!exerciseRecord[key].includes(date)) {
+    //             exerciseRecord[key] = [...exerciseRecord[key], date, 0]
+
+
+    //         }
+    //     }
+
+    // })
+
+
+    // for (let key in exerciseRecord) {
+    //     let unOrganizedDates = exerciseRecord[key]
+    //     let organizedDates = []
+    //     let finalDates = []
+    //     console.log('LOOKIE HERE SON', unOrganizedDates)
+
+    //     for (let i = 0; i < unOrganizedDates.length - 1; i += 2) {
+    //         organizedDates.push({ date: unOrganizedDates[i], score: unOrganizedDates[i + 1] })
+    //     }
+    //     organizedDates.sort((a, b) => new Date(a.date) - new Date(b.date))
+    //     console.log('new Object? ', organizedDates)
+
+    //     for (let i = 0; i < organizedDates.length; i++) {
+    //         let date = organizedDates[i].date
+    //         let score = organizedDates[i].score
+    //         finalDates.push(date)
+    //         finalDates.push(score)
+    //     }
+    //     console.log('final Object? ', finalDates)
+    //     exerciseRecord[key] = finalDates
+
+    // }
+
+
+    // let unsortedDates = needsSorting.map
+    // needsSorting.sort((a, b) => new Date(a.date) - new Date(b.date))
+    // exerciseRecord[key] = needsSorting
+
+    console.log('object reorganized?', exerciseRecord)
+
+
 
 
     const data = {
@@ -189,3 +307,56 @@ function GraphProgress() {
 }
 
 export default GraphProgress;
+
+
+// sortedData.forEach(record => {
+//     if (!exerciseRecord[record.workout1] && record.workout1 != ' ' && record.workout1 != '') {
+//         exerciseRecord[record.workout1] = { date: record.date, score: record.workout1Score };
+//     } else if (exerciseRecord[record.workout1] && record.workout1 != ' ' && record.workout1 != '') {
+//         let workout1 = record.workout1;
+//         exerciseRecord[workout1] = { ...exerciseRecord[workout1], date: record.date, score: record.workout1Score };
+//     }
+
+//     if (!exerciseRecord[record.workout2] && record.workout2 != ' ' && record.workout2 != '') {
+//         exerciseRecord[record.workout2] = { date: record.date, score: record.workout2Score };
+//     } else if (exerciseRecord[record.workout2] && record.workout2 != ' ' && record.workout2 != '') {
+//         exerciseRecord[record.workout2] = { ...exerciseRecord[record.workout2], date: record.date, score: record.workout2Score };
+//     }
+
+//     if (!exerciseRecord[record.workout3] && record.workout3 != ' ' && record.workout3 != '') {
+//         exerciseRecord[record.workout3] = { date: record.date, score: record.workout3Score };
+//     } else if (exerciseRecord[record.workout3] && record.workout3 != ' ' && record.workout3 != '') {
+//         exerciseRecord[record.workout3] = { ...exerciseRecord[record.workout3], date: record.date, score: record.workout3Score };
+//     }
+
+//     if (!exerciseRecord[record.workout4] && record.workout4 != ' ' && record.workout4 != '') {
+//         exerciseRecord[record.workout4] = { date: record.date, score: record.workout4Score };
+//     } else if (exerciseRecord[record.workout4] && record.workout4 != ' ' && record.workout4 != '') {
+//         exerciseRecord[record.workout4] = { ...exerciseRecord[record.workout4], date: record.date, score: record.workout4Score };
+//     }
+
+//     if (!exerciseRecord[record.workout5] && record.workout5 != ' ' && record.workout5 != '') {
+//         exerciseRecord[record.workout5] = { date: record.date, score: record.workout5Score };
+//     } else if (exerciseRecord[record.workout5] && record.workout5 != ' ' && record.workout5 != '') {
+//         exerciseRecord[record.workout5] = { ...exerciseRecord[record.workout5], date: record.date, score: record.workout5Score };
+//     }
+
+//     if (!exerciseRecord[record.workout6] && record.workout6 != ' ' && record.workout6 != '') {
+//         exerciseRecord[record.workout6] = { date: record.date, score: record.workout6Score };
+//     } else if (exerciseRecord[record.workout6] && record.workout6 != ' ' && record.workout6 != '') {
+//         exerciseRecord[record.workout6] = { ...exerciseRecord[record.workout6], date: record.date, score: record.workout6Score };
+//     }
+
+//     if (!exerciseRecord[record.workout7] && record.workout7 != ' ' && record.workout7 != '') {
+//         exerciseRecord[record.workout7] = { date: record.date, score: record.workout7Score };
+//     } else if (exerciseRecord[record.workout7] && record.workout7 != ' ' && record.workout7 != '') {
+//         exerciseRecord[record.workout7] = { ...exerciseRecord[record.workout7], date: record.date, score: record.workout7Score };
+//     }
+
+//     if (!exerciseRecord[record.workout8] && record.workout8 != ' ' && record.workout8 != '') {
+//         exerciseRecord[record.workout8] = { date: record.date, score: record.workout8Score };
+//     } else if (exerciseRecord[record.workout8] && record.workout8 != ' ' && record.workout8 != '') {
+//         exerciseRecord[record.workout8] = { ...exerciseRecord[record.workout8], date: record.date, score: record.workout8Score };
+//     }
+
+// })
